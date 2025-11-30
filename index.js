65b480f0514ad5c91772e196d6a7b829e161b987
GeoCarter96 <geolcarter21@gmail.com>
2025-11-30 16:34:36 -0500

    add

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 //https://www.omdbapi.com/?i=tt3896198&apikey=d69e1a3c&s=dark
 
 async function main() {
-    const movie = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=d69e1a3c&s=dark");
-    const movies = await movie.();
-    console.log(
-    movies.map((film) => 
-     `<div class="search-controls">
-<button id="searchBtn">Search</button>
-<select id="sortSelect" class="sort-select">
-    <option value="" disabled="" selected="">Sort movies...</option>
-    <option value="az">Title A → Z</option>
-    <option value="za">Title Z → A</option>
-    <option value="newest">Newest First</option>
-    <option value="oldest">Oldest First</option>
-</select>
-</div>`)
-    );
+    const movie = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=d69e1a3c");
+    const movies = await movie.json();
+    const movieList = document.querySelector('.search-controls');
+    
+    movieList.innerHTML = movies.Search.map((film) => 
+     `<div class="movie-card">
+      <img src="${film.poster}" alt="">
+      <h3>${film.title}</h3>
+      <p>${film.year}</p>
+    </div>
+    <div class="movie-card">
+      <img src="${film.poster}" alt="">
+      <h3>${film.title}</h3>
+      <p>${film.year}</p>
+    </div>
+    <div class="movie-card">
+      <img src="${film.poster}" alt="">
+      <h3>${film.title}</h3>
+      <p>${film.year}</p>
+    </div>
+    <div class="movie-card">
+      <img src="${film.poster}" alt="">
+      <h3>${film.title}</h3>
+      <p>${film.year}</p>
+    </div>
+    <div class="movie-card">
+      <img src="${film.poster}" alt="">
+      <h3>${film.title}</h3>
+      <p>${film.year}</p>
+    </div>
+    <div class="movie-card">
+      <img src="${film.poster}" alt="">
+      <h3>${film.title}</h3>
+      <p>${film.year}</p>
+    </div>`).join("");
+    
 }
 
 main();
